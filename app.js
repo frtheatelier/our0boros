fd6b007ae17ef9ecb6883aa3706e914bbc1f4462
Kimberly Prijadi <prijadikimberly@gmail.com>
2025-08-17 00:25:37 +0700

    used iframe for attributions and update/changelogs

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+// CURRENT DATE
+var d = new Date();
+var dateString = d.toDateString();
+var timeString = d.toTimeString();
+document.getElementById("currdate").innerHTML = dateString;
+document.getElementById("currtime").innerHTML = timeString;
+
+// WEB COUNTER - https://dannarchy.com/tut/tut_002
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var site_data = JSON.parse(this.responseText);
+        console.log("success");
+        // document.write(site_data.info.sitename, site_data.info.views)
+        document.getElementById("hitcount").innerHTML = site_data.info.views;
+        // technically react or vue or svelte or some other would be neater lol
+    } else {
+        console.log("you wen ti :(");
+    }
+};
+xhttp.open("GET", "https://weirdscifi.ratiosemper.com/neocities.php?sitename=our0boros", true);
+xhttp.send();
+
 // RIPPLE EFFECT 
 
 let lastEvent = null;
@@ -31,7 +55,7 @@ console.log(":D", hoverable);
 
 let intervalId = null;
 
-// RIPPLE EFFECT
+// RIPPLE EFFECT EVENT LISTENER
 
 // hoverable.forEach(element => {
 //     element.addEventListener('mouseenter', function(e) {
@@ -51,3 +75,25 @@ let intervalId = null;
 //         element.querySelectorAll(".hover-circle").forEach(c => c.remove());
 //     })
 // });
+
+// SHOW ATTR AND LOG
+
+function displayAttr() {
+    var attr = document.getElementById("attributionIFRWrapper")
+
+    if (attr.style.display == 'none') {
+        attr.style.display = 'block';
+    } else {
+        attr.style.display = 'none';
+    }
+}
+
+function displayUpdates() {
+    var updt = document.getElementById("updateIFRWrapper")
+
+    if (updt.style.display == 'none') {
+        updt.style.display = 'block';
+    } else {
+        updt.style.display = 'none';
+    }
+}
